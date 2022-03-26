04b117cbe893a0b99f27feb338d593d50d6a25c8
Dom <domant3lis@protonmail.com>
2022-03-26 21:36:50 +0200

    Implemented time calculation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is a tool to generate PSP (Personal software process) markdown and csv documents from git commits.
 
 ## Dependencies
-`ruby`, `git` 
+`ruby`, `git`, `libgit`
 Ruby gems: [`git`](https://rubygems.org/gems/git), [`csv2md`](https://rubygems.org/gems/csv2md), [`csv_shaper`](https://rubygems.org/gems/csv_shaper)
 
 ## Usage
@@ -12,8 +12,8 @@ This tool assumes that commits are made often and each small improvement is in a
 Inside commit messages at the end include this:
 ```
 --- PSP ---
-From: HH:MM
-Interruptions: 3 (Coffee break), X (Reason)
+From: HH:MM or YYYY-MM-DD HH:MM
+Interruptions: 3 (Coffee break); MM (Reason)
 Action: Adding tests
 Custom 0: Some text
 Custom 1: Some text
@@ -32,11 +32,13 @@ These fields are not required, but they are treated in a special way, they are u
 
 I may or may not implement these improvements, listed with no priority in mind:
 
-* Correct time calculation across days (for example: `From: 23:30 Till: 00:30`) 
-* Merge columns (i.e. custom fields) (useful for typos in commits)
-* Correct time calculation with different time zones
+* Improve how fields are parsed; it's janky
+* Overhaul how time is calculated, currently it is also very janky:
+	- Correct time calculation across days (for example: `From: 23:30 Till: 00:30`) 
+	- Correct time calculation with different time zones
 * Collect data from a specific person(s) which made the commits
 * Produce an intermediary CSV file, switch could be edited manually and then do time calculation
+* Ctrl+F for TODOs arround the code for more
 
 ## Related documentation
 <https://ruby-doc.org/stdlib-2.6.1/libdoc/csv/rdoc/CSV.html>
--- psp.rb
+++ psp.rb
@@ -1,154 +1,255 @@
 require 'optparse'
 require 'set'
 require 'git'
-require 'csv_shaper'
 require 'csv2md'
+require 'csv_shaper'
+require 'time'
 
-options = { 
-  :from => 0, 
-  :till => 100,
-  :markdown => 'psp.md',
-  :csv => nil,
-  :markdown_out => nil,
-  :csv_out => nil,
-}
+class PSP
+  attr_accessor :options
+  attr_reader :repo, :data, :header
 
-OptionParser.new do |parser|
-  parser.banner = "Usage: psp.rb [options]"
+  def initialize
+    @options = {
+      repopath: nil,
+      since_commit: 0,
+      till_commit: 100,
+      markdown_fileout: nil,
+      csv_fileout: nil,
+      markdown_stdout: nil,
+      csv_stdout: nil
+      # TODO: Implement these options
+      # csv_filein: nil,
+      # :include_commiters => [],
+      # :exclude_commiters => [],
+      # :include_commits => [],
+      # :exclude_commits => [],
+      # :since_date => nil,
+      # :till_date => nil,
+    }
 
-  parser.on("-oPATH", "--open=PATH", "Open local repo in that PATH") do |arg|
-    options[:path] = arg
+    parse_opts()
+
+    if !@options[:csv_fileout] && !@options[:markdown_fileout] && !@options[:markdown_stdout] && !@options[:csv_stdout]
+      puts 'No output options specified'
+      exit
+    end
+
+    open_local_repo()
+    parse_commits()
+
+    @csv_string = to_csv()
+    @markdown_string = Csv2md.new(@csv_string).gfm
+
+    if @options[:csv_fileout]
+      file = File.new(options[:csv_fileout], 'w')
+      file.syswrite(@csv_string)
+    end
+
+    if @options[:markdown_fileout]
+      file = File.new(options[:markdown_fileout], 'w')
+      file.syswrite(@markdown_string)
+    end
+
+    if @options[:markdown_stdout]
+      puts "#{@markdown_string}"
+    end
+
+    if @options[:csv_stdout]
+      puts "#{@csv_string}"
+    end
   end
 
-  parser.on("-fFROM", "--from=FROM", "Specify from which commit to generate, default is 0 (i.e. newest)") do |arg|
-    options[:from] = arg
+  def open_local_repo
+    @repo = Git.open(@options[:repopath])
+  rescue StandardError
+    puts 'Please, provide correct path to your repo'
+    exit
   end
 
-  parser.on("-tTILL", "--till=TILL", "Specify till which commit to generate, default is 100 (i.e. till newest 100th commit)") do |arg|
-    options[:till] = arg
-  end
+  def to_csv
+    # # This is, as of yet, not supported,
+    # # because `csv2md` cannot deal with
+    # # other separators
+    #
+    # CsvShaper.configure do |config|
+    #   config.col_sep = "\t"
+    # end
 
-  parser.on("-mFILEPATH", "--markdown=FILEPATH", "Specify where to export the markdown file, default './psp.md'") do |arg|
-    options[:markdown] = arg
-  end
+    # Builds a cvs out of data
+    csv = CsvShaper::Shaper.new
 
-  parser.on("-cFILEPATH", "--csv=FILEPATH", "Specify where to export the csv file, by default it isn't exported") do |arg|
-    options[:csv] = arg
-  end
-
-  parser.on("-dm", "--display-markdown", "Displays markdown output to stdout") do
-    puts "AAA"
-    options[:markdown_out] = true
-  end
-
-  parser.on("-dc", "--display-csv", "Displays cvs output to stdout") do
-    puts "AAA"
-    options[:csv_out] = true
-  end
-
-end.parse!
-
-# p options
-
-begin
-  repo = Git.open(options[:path])
-rescue
-  puts "Please, provide correct path to your repo"
-  exit
-end
-
-logs = repo.log
-
-# Presets
-header = Set["Date", "From", "Till", "Interruptions", "Overall", "Action", "Details", "Changes"]
-
-data = []
-
-# Finds all headers and data
-logs[options[:from]..options[:till]].each do |log|
-
-  if log.commit?
-    row = Hash.new()
-
-    row["Date".to_sym] = log.committer_date.to_s[5..9]
-    row["Till".to_sym] = log.committer_date.to_s[11..15]
-    row["Overall".to_sym] = "TODO"
-    row["Changes".to_sym] = "-#{ log.diff_parent.deletions.to_int.to_s } +#{ log.diff_parent.lines.to_int.to_s } ~#{ log.diff_parent.insertions.to_int.to_s }"
-
-    # PSP parsing
-    commit_msg = log.message.split("--- PSP ---")
-    row["Details"] = commit_msg[0].split("\n").join(" ")
-
-    if commit_msg[1]
-      fields = commit_msg[1].split("\n")
-
-      fields[1..].each do |field|
-        key = field.split(":")[0]
-        value = field.split(":")[1..].join(':')
-
-        header.add key
-        
-        row[key] = value
-
-        # TODO: time calculations
-        if row["From"]
-          if row["Interruptions"]
-            # TODO: Calculations
-          end
-        end
-
+    # Builds a header
+    csv.headers @header do |csv, _head|
+      header.each do |head|
+        csv.columns head
       end
     end
 
-    data.push(row)
-  end
-end
-
-header = header - [nil]
-
-# # This is, as of yet, not supported,
-# # because `csv2md` cannot deal with
-# # other separators
-# 
-# CsvShaper.configure do |config|
-#   config.col_sep = "\t"
-# end
-
-# Builds a cvs out of data
-csv = CsvShaper::Shaper.new
-
-# Builds a header
-csv.headers header do |csv, head|
-  header.each do |head|
-    csv.columns head
-  end
-end
-
-# Builds the rest of data
-data.each do |data_row|
-  csv.row do |csv|
-    data_row.each do |data_cell|
-      csv.cell data_cell[0], data_cell[1]
+    # Builds the rest of data
+    @data.each do |data_row|
+      csv.row do |csv|
+        data_row.each do |data_cell|
+          csv.cell data_cell[0], data_cell[1]
+        end
+      end
     end
+
+    csv.to_csv
+  end
+
+  private def parse_commits
+    logs = @repo.log
+
+    # Presets
+    @header = Set[
+      'Date',
+      'From',
+      'Till',
+      'Interruptions',
+      'Overall',
+      'Action',
+      'Details',
+      'Changes',
+    ]
+
+    @data = []
+
+    # Finds all headers and data
+    logs[@options[:from]..@options[:till]].each do |log|
+      next unless log.commit?
+
+      row = {}
+
+      row[:Date] = log.committer_date.to_s[5..9]
+      row[:Till] = log.committer_date.to_s[11..15]
+      row[:Changes] = "-#{log.diff_parent.deletions.to_int} +#{log.diff_parent.lines.to_int} ~#{log.diff_parent.insertions.to_int}"
+
+      # PSP parsing
+      commit_msg = log.message.split('--- PSP ---')
+      unless commit_msg[1]
+        commit_msg = log.message.split('---PSP---')
+      end
+        
+      row['Details'] = commit_msg[0].split("\n").join(' ')
+      
+      # puts "COMMIT_MSG: " + commit_msg.to_s
+      
+      # Parses the rest of the fields
+      if commit_msg[1]
+        fields = commit_msg[1].split("\n")
+
+        fields[1..].each do |field|
+          key = field.split(':')[0]
+          value = field.split(':')[1..].join(':')
+
+          @header.add key
+
+          row[key] = value
+        end
+      end
+      
+      # puts "#{row}"
+      # puts "#{row['From']}"
+      
+      # puts "ROW: " + row.to_s + "\n\n"
+      # Calculates time spend on a commit
+      if row['From']
+        
+        begin
+          date_from = DateTime.strptime(row['From'], " %H:%M")
+        rescue
+          date_from = nil
+          puts "Failed to parse an `From` field from commit '#{log.sha}' From: `#{row['From']}`"
+        end
+        
+        # TODO: Remove this, it is kept to support some old commit messages in mine projects
+        if row['Interferences']
+          row['Interruptions'] = row['Interferences']
+        end
+        
+        commit_time = log.committer_date
+        date_from = Time.new(commit_time.year, commit_time.mon, commit_time.day, date_from.hour, date_from.min,0)
+        
+        time = commit_time.to_i
+        time -= date_from.to_i
+        
+        # Parses interruptions
+        if row['Interruptions'] && date_from
+          date_int = []
+          
+          # TODO: Remove this, it is kept to support some old commit messages in mine project
+          interruptions = row['Interruptions'].split(';')
+          unless interruptions[1]
+            interruptions = row['Interruptions'].split(',')
+          end
+          
+          interruptions.each do |int|
+            int_ = int.split('(')
+            begin
+              # Parses "%M"
+              date_int.push(Integer(int_[0]) * 60)
+            rescue
+              begin
+                # Parses "%H:%M"
+                date_int.push(Integer(int_.split(':')[0] * 60 * 60, 60 * Integer(int_.split(':')[1])))
+              rescue
+                puts "Failed to parse an interruption from commit '#{log.sha}' `#{int}`"
+                next
+              end
+            end
+          end
+          
+          date_int.each do |int|
+            time -= int.to_i
+          end
+        end
+          
+        # Convertion into minutes
+        row['Overall'] = time / 60
+      end
+      @data.push(row)
+    end
+    @header -= [nil]
+  end
+
+  private def parse_opts
+    OptionParser.new do |parser|
+      parser.banner = 'Usage: psp.rb [options]'
+
+      parser.on('-rPATH', '--repo=PATH', 'Open local repo in that PATH') do |arg|
+        @options[:repopath] = arg
+      end
+
+      parser.on('--since-commit=SINCE', Integer, 'Specify from which commit to generate, default is 0 (i.e. newest)') do |arg|
+        @options[:since_commit] = arg
+      end
+
+      parser.on('--till-commit=TILL', 'Specify till which commit to generate, default is 100 (i.e. till newest 100th commit)') do |arg|
+        @options[:till_commit] = arg
+      end
+
+      parser.on('-mFILEPATH', '--markdown=FILEPATH', 'Specify where to export the markdown file') do |arg|
+        @options[:markdown_fileout] = arg
+      end
+
+      parser.on('-cFILEPATH', '--csv=FILEPATH', 'Specify where to export the csv file') do |arg|
+        @options[:csv_fileout] = arg
+      end
+
+      parser.on('--display-markdown', 'Displays markdown output to stdout') do
+        @options[:markdown_stdout] = true
+      end
+
+      parser.on('--display-csv', 'Displays cvs output to stdout') do
+        @options[:csv_stdout] = true
+      end
+
+      # parser.on("--csv-in=FILEPATH", "parses") do |arg|
+      #   @options[:csv_fileout] = arg
+      # end
+    end.parse!
   end
 end
 
-csv_string = csv.to_csv
-markdown_string = Csv2md.new(csv_string).gfm
-
-if options[:csv_out]
-  puts "CSV:\n" + csv_string + "\n"
-end
-
-if options[:markdown_out]
-  puts "Markdown:\n" + markdown_string + "\n"
-end
-
-if options[:csv] != nil
-  file = File.new(options[:csv], "w")
-  file.syswrite(csv_string)
-  puts ""
-end
-
-file = File.new(options[:markdown], "w")
-file.syswrite(markdown_string)
+PSP.new
