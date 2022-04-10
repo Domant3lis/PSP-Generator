e48bedb7007431d2cc4bc23f6c1c6171247d69eb
Dom <domant3lis@protonmail.com>
2022-04-10 17:26:24 +0300

    Major overhaul
    I've split functionallity into two separate files. Commit filtering and
    parsing is now done by passing blocks. Now, it should be much easier to
    add new functionallity

---
+++ app.rb
@@ -0,0 +1,276 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'csv2md'
+require './psp'
+require 'csv_shaper'
+require 'time'
+
+@options = {
+	repopath: nil,
+	# since_commit: nil,
+	# till_commit: nil,
+	# markdown_fileout: nil,
+	csv_fileout: nil,
+	# markdown_stdout: nil,
+	csv_stdout: nil,
+	csv_filein: nil,
+	tags: nil,
+	syntax: 'psp',
+}
+
+OptionParser.new do |parser|
+	parser.banner = 'Usage: psp.rb [options]'
+
+	parser.on('-rPATH', '--repo=PATH', 'Open local repo in that PATH') do |arg|
+		@options[:repopath] = arg
+	end
+
+	parser.on('-cFILEPATH', '--csv=FILEPATH', 'Specify where to export the csv file') do |arg|
+		@options[:csv_fileout] = arg
+	end
+
+	parser.on('--display-csv', 'Displays cvs output to stdout') do
+		@options[:csv_stdout] = true
+	end
+
+	parser.on('--syntax=SYNTAX', 'Choose in which mode to parse your commits, supported: psp, WIP: lff') do |arg|
+		@options[:syntax] = arg
+	end
+
+	parser.on('--tags=TAGS', 'Specify which commits with tags to include') do |arg|
+		@options[:tags] = arg.split('; ')
+	end
+
+	# parser.on('--since-commit=SINCE', Integer, 'Specify from which commit to generate, default is 0 (i.e. newest)') do |arg|
+	# 	@options[:since_commit] = arg
+	# end
+
+	# parser.on('--till-commit=TILL', 'Specify till which commit to generate, default is 100 (i.e. till newest 100th commit)') do |arg|
+	# 	@options[:till_commit] = arg
+	# end
+
+	# parser.on('--csv-in=FILEPATH', 'TODO') do |arg|
+	# 	@options[:csv_filein] = arg
+	# end
+
+	# parser.on('-mFILEPATH', '--markdown=FILEPATH', 'Specify where to export the markdown file') do |arg|
+	# 	@options[:markdown_fileout] = arg
+	# end
+
+	# parser.on('--display-markdown', 'Displays markdown output to stdout') do
+	# 	@options[:markdown_stdout] = true
+	# end
+end.parse!
+
+puts(@options[:repopath])
+
+psp = PSP.new(@options[:repopath])
+
+puts(@options[:tags])
+
+psp.read_commits do |log|
+	commit_info = {}
+
+	commit_msg = log.message.split(/-+ *PSP *-+/)
+	commit_info['Details'] = commit_msg[0].split(/\n/).join(' ').split(/\t/).join(' ')
+	commit_info[:itself] = log
+
+	if commit_msg[1]
+
+		fields = commit_msg[1].split("\n")
+
+		fields[1..].each do |field|
+			temp = field.split(':')
+
+			key = temp[0]
+			value = temp[1..].join(':')
+
+			commit_info[key] = value
+		end
+	end
+
+	commit_info
+end
+
+if @options[:tags]
+	psp.filter_commits do |commit|
+		includes = false
+
+		@options[:tags].any? do |t|
+			if commit['Tag']
+				includes = commit['Tag'].include? t
+			end
+		end
+
+		# Todo exclude / include commiters
+		# includes = commit[:itself].commiters.any? @options[:commiters]
+
+		# Todo exclude / include commits
+
+		includes
+	end
+end
+
+psp.on('Changes') do |commit_info|
+	commit_info['Changes'] = "-#{commit_info['Changes'].deletions.to_int} +#{commit_info['Changes'].lines.to_int} ~#{commit_info['Changes'].insertions.to_int}"
+	commit_info
+end
+
+psp.on('Till') do |commit|
+	begin
+		date_till = Time.strptime(commit['Till'], ' %H:%M')
+
+		date_till = Time.new(commit['Date'].year, commit['Date'].mon, commit['Date'].day, date_till.hour, date_till.min)
+
+		commit['Date'] = date_till
+	rescue StandardError
+		puts("Failed to parse field 'Till' in commit #{commit_info['itself']}: #{commit_info['Till']}")
+	end
+
+	commit
+end
+
+psp.on('From') do |commit_info|
+	begin
+		date_from = Time.strptime(commit_info['From'], ' %H:%M')
+
+		date_from = Time.new(commit_info['Date'].year, commit_info['Date'].mon, commit_info['Date'].day, date_from.hour, date_from.min)
+
+		commit_info['From'] = date_from
+
+		commit_info['Overall'] = (commit_info['Date'].to_i - commit_info['From'].to_i) / 60
+	rescue StandardError
+		puts("Failed to parse field 'From' in commit #{commit_info['itself']}: #{commit_info['From']}")
+	end
+
+	commit_info
+end
+
+psp.on('From', 'Interruptions') do |commit|
+	begin
+		time = commit['Date'].to_i
+		time -= commit['From'].to_i
+
+		# TODO: Remove this, it is kept to support some old commit messages in my project
+		# interruptions = row['Interruptions'].split(';')
+		# unless interruptions[1]
+		# 	interruptions = row['Interruptions'].split(',')
+		# end
+
+		interruptions = commit['Interruptions'].split(';')
+
+		ints = []
+
+		interruptions.each do |int|
+			int_ = int.split('(')
+			begin
+				# Parses "%M"
+				ints.push(Integer(int_[0]) * 60)
+			rescue StandardError
+				begin
+					# Parses "%H:%M"
+					ints.push(Integer(int_.split(':')[0] * 60 * 60, 60 * Integer(int_.split(':')[1])))
+				rescue StandardError
+					puts "Failed to parse an interruption from commit '#{row[:itself].sha}' `#{int}`"
+					next
+				end
+			end
+		end
+
+		ints.each do |int|
+			time -= int.to_i
+		end
+
+		commit['Overall'] = time / 60
+	rescue StandardError
+		puts("Failed to parse fields 'From' and 'Interruptions' in commit #{commit['itself']}: #{commit['From']}, #{commit['Interruptions']}")
+	end
+	commit
+end
+
+default_preset = Set[
+	'Date',
+	'From',
+	'Till',
+	'Interruptions',
+	'Overall',
+	'Action',
+	'Details',
+	'Changes',
+]
+
+csv_string = psp.to_csv(exclude: ['Tag'], preset: default_preset, col_sep: "\t")
+# csv_string = psp.to_csv(col_sep: "\t")
+
+if @options[:csv_stdout]
+	puts(csv_string)
+end
+
+if @options[:csv_fileout]
+	file = File.new(@options[:csv_fileout], 'w')
+	file.syswrite(csv_string)
+end
+
+	# def parse_data
+	# 	@data.map! do |row|
+
+	# 		if row['From']
+	# 			begin
+	# 				# puts(row['From'])
+	# 				date_from = DateTime.strptime(row['From'], ' %H:%M')
+
+	# 				# TODO: Remove this, it is kept to support some old commit messages in mine projects
+	# 				if row['Interferences']
+	# 					row['Interruptions'] = row['Interferences']
+	# 				end
+					
+	# 				commit_time = row['Date']
+	# 				date_from = Time.new(commit_time.year, commit_time.mon, commit_time.day, date_from.hour, date_from.min, 0)
+
+	# 				# if row['Till']
+	# 				#   puts
+	# 				#   time =
+	# 				# else
+	# 				# end
+
+	# 				# Parses interruptions
+	# 				if row['Interruptions'] && date_from
+	# 					date_int = []
+
+	# 					# TODO: Remove this, it is kept to support some old commit messages in mine project
+	# 					interruptions = row['Interruptions'].split(';')
+	# 					unless interruptions[1]
+	# 						interruptions = row['Interruptions'].split(',')
+	# 					end
+
+	# 					interruptions.each do |int|
+	# 						int_ = int.split('(')
+	# 						begin
+	# 							# Parses "%M"
+	# 							date_int.push(Integer(int_[0]) * 60)
+	# 						rescue StandardError
+	# 							begin
+	# 								# Parses "%H:%M"
+	# 								date_int.push(Integer(int_.split(':')[0] * 60 * 60, 60 * Integer(int_.split(':')[1])))
+	# 							rescue StandardError
+	# 								puts "Failed to parse an interruption from commit '#{row[:itself].sha}' `#{int}`"
+	# 								next
+	# 							end
+	# 						end
+	# 					end
+
+	# 					date_int.each do |int|
+	# 						time -= int.to_i
+	# 					end
+	# 				end
+
+	# 				# Convertion into minutes
+	# 				row['Overall'] = time / 60
+	# 			rescue StandardError
+	# 				puts "Failed to parse an `From` field from commit '#{row[:itself].sha}' From: `#{row['From']}`"
+	# 			end
+	# 		end
+	# 		row
+	# 	end
+	# end
--- psp.rb
+++ psp.rb
@@ -1,255 +1,95 @@
-require 'optparse'
+#!/usr/bin/env ruby
+
 require 'set'
 require 'git'
-require 'csv2md'
-require 'csv_shaper'
-require 'time'
 
 class PSP
-  attr_accessor :options
-  attr_reader :repo, :data, :header
+	attr_reader :repo, :data
 
-  def initialize
-    @options = {
-      repopath: nil,
-      since_commit: 0,
-      till_commit: 100,
-      markdown_fileout: nil,
-      csv_fileout: nil,
-      markdown_stdout: nil,
-      csv_stdout: nil
-      # TODO: Implement these options
-      # csv_filein: nil,
-      # :include_commiters => [],
-      # :exclude_commiters => [],
-      # :include_commits => [],
-      # :exclude_commits => [],
-      # :since_date => nil,
-      # :till_date => nil,
-    }
+	def initialize(repopath)
+		open_local_repo(repopath)
 
-    parse_opts()
+		@data = []
+	end
 
-    if !@options[:csv_fileout] && !@options[:markdown_fileout] && !@options[:markdown_stdout] && !@options[:csv_stdout]
-      puts 'No output options specified'
-      exit
-    end
+	def open_local_repo(repopath)
+		@repo = Git.open(repopath)
+	rescue StandardError
+		puts 'Failed to open repo, specified path may be incorect'
+		exit
+	end
 
-    open_local_repo()
-    parse_commits()
+	def read_commits(&syntax)
+		logs = @repo.log
 
-    @csv_string = to_csv()
-    @markdown_string = Csv2md.new(@csv_string).gfm
+		logs = logs.select { |c| c.commit? }
 
-    if @options[:csv_fileout]
-      file = File.new(options[:csv_fileout], 'w')
-      file.syswrite(@csv_string)
-    end
+		logs.each do |log|
+			commit_info = {}
 
-    if @options[:markdown_fileout]
-      file = File.new(options[:markdown_fileout], 'w')
-      file.syswrite(@markdown_string)
-    end
+			commit_info['Date'] = log.committer_date
+			commit_info['Changes'] = log.diff_parent
 
-    if @options[:markdown_stdout]
-      puts "#{@markdown_string}"
-    end
+			commit_info.merge! syntax.call(log)
+			@data.push(commit_info)
+		end
+	end
 
-    if @options[:csv_stdout]
-      puts "#{@csv_string}"
-    end
-  end
+	def on(*keys, &block)
+		@data.map! do |commit_info|
+			if keys.all? { |k| commit_info.key?(k) }
+				commit_info = commit_info.merge block.call(commit_info)
+			end
 
-  def open_local_repo
-    @repo = Git.open(@options[:repopath])
-  rescue StandardError
-    puts 'Please, provide correct path to your repo'
-    exit
-  end
+			commit_info
+		end
+	end
 
-  def to_csv
-    # # This is, as of yet, not supported,
-    # # because `csv2md` cannot deal with
-    # # other separators
-    #
-    # CsvShaper.configure do |config|
-    #   config.col_sep = "\t"
-    # end
+	def filter_commits(&filter)
+		@data.select! { |c| filter.call(c) }
+	end
 
-    # Builds a cvs out of data
-    csv = CsvShaper::Shaper.new
+	def filter_parsed(&_filter); end
 
-    # Builds a header
-    csv.headers @header do |csv, _head|
-      header.each do |head|
-        csv.columns head
-      end
-    end
+	def build_header(preset, exclude: [])
+		@data.each do |row|
+			exclude.each { |e| row.delete(e) }
+			preset += row.keys.to_set
+		end
+		preset - [nil, :itself]
+	end
 
-    # Builds the rest of data
-    @data.each do |data_row|
-      csv.row do |csv|
-        data_row.each do |data_cell|
-          csv.cell data_cell[0], data_cell[1]
-        end
-      end
-    end
+	def to_csv(exclude: [], preset: Set[], col_sep: "\t")
+		exclude.push :itself
+		header = build_header(preset, exclude: exclude)
 
-    csv.to_csv
-  end
+		# Be wary `csv2md` cannot deal with
+		# other separators than '\t'
+		CsvShaper.configure do |config|
+			config.col_sep = col_sep
+		end
 
-  private def parse_commits
-    logs = @repo.log
+		csv_shapper = CsvShaper::Shaper.new
 
-    # Presets
-    @header = Set[
-      'Date',
-      'From',
-      'Till',
-      'Interruptions',
-      'Overall',
-      'Action',
-      'Details',
-      'Changes',
-    ]
+		# Builds a header
+		csv_shapper.headers header do |csv, _head|
+			header.each do |head|
+				csv.columns head
+			end
+		end
 
-    @data = []
+		# Builds the rest of data
+		@data.each do |data_row|
+			exclude.each { |e| data_row.delete(e) }
 
-    # Finds all headers and data
-    logs[@options[:from]..@options[:till]].each do |log|
-      next unless log.commit?
+			csv_shapper.row do |csv|
+				data_row.each do |data_cell|
+					csv.cell data_cell[0], data_cell[1]
+				end
+			end
+		end
 
-      row = {}
-
-      row[:Date] = log.committer_date.to_s[5..9]
-      row[:Till] = log.committer_date.to_s[11..15]
-      row[:Changes] = "-#{log.diff_parent.deletions.to_int} +#{log.diff_parent.lines.to_int} ~#{log.diff_parent.insertions.to_int}"
-
-      # PSP parsing
-      commit_msg = log.message.split('--- PSP ---')
-      unless commit_msg[1]
-        commit_msg = log.message.split('---PSP---')
-      end
-        
-      row['Details'] = commit_msg[0].split("\n").join(' ')
-      
-      # puts "COMMIT_MSG: " + commit_msg.to_s
-      
-      # Parses the rest of the fields
-      if commit_msg[1]
-        fields = commit_msg[1].split("\n")
-
-        fields[1..].each do |field|
-          key = field.split(':')[0]
-          value = field.split(':')[1..].join(':')
-
-          @header.add key
-
-          row[key] = value
-        end
-      end
-      
-      # puts "#{row}"
-      # puts "#{row['From']}"
-      
-      # puts "ROW: " + row.to_s + "\n\n"
-      # Calculates time spend on a commit
-      if row['From']
-        
-        begin
-          date_from = DateTime.strptime(row['From'], " %H:%M")
-        rescue
-          date_from = nil
-          puts "Failed to parse an `From` field from commit '#{log.sha}' From: `#{row['From']}`"
-        end
-        
-        # TODO: Remove this, it is kept to support some old commit messages in mine projects
-        if row['Interferences']
-          row['Interruptions'] = row['Interferences']
-        end
-        
-        commit_time = log.committer_date
-        date_from = Time.new(commit_time.year, commit_time.mon, commit_time.day, date_from.hour, date_from.min,0)
-        
-        time = commit_time.to_i
-        time -= date_from.to_i
-        
-        # Parses interruptions
-        if row['Interruptions'] && date_from
-          date_int = []
-          
-          # TODO: Remove this, it is kept to support some old commit messages in mine project
-          interruptions = row['Interruptions'].split(';')
-          unless interruptions[1]
-            interruptions = row['Interruptions'].split(',')
-          end
-          
-          interruptions.each do |int|
-            int_ = int.split('(')
-            begin
-              # Parses "%M"
-              date_int.push(Integer(int_[0]) * 60)
-            rescue
-              begin
-                # Parses "%H:%M"
-                date_int.push(Integer(int_.split(':')[0] * 60 * 60, 60 * Integer(int_.split(':')[1])))
-              rescue
-                puts "Failed to parse an interruption from commit '#{log.sha}' `#{int}`"
-                next
-              end
-            end
-          end
-          
-          date_int.each do |int|
-            time -= int.to_i
-          end
-        end
-          
-        # Convertion into minutes
-        row['Overall'] = time / 60
-      end
-      @data.push(row)
-    end
-    @header -= [nil]
-  end
-
-  private def parse_opts
-    OptionParser.new do |parser|
-      parser.banner = 'Usage: psp.rb [options]'
-
-      parser.on('-rPATH', '--repo=PATH', 'Open local repo in that PATH') do |arg|
-        @options[:repopath] = arg
-      end
-
-      parser.on('--since-commit=SINCE', Integer, 'Specify from which commit to generate, default is 0 (i.e. newest)') do |arg|
-        @options[:since_commit] = arg
-      end
-
-      parser.on('--till-commit=TILL', 'Specify till which commit to generate, default is 100 (i.e. till newest 100th commit)') do |arg|
-        @options[:till_commit] = arg
-      end
-
-      parser.on('-mFILEPATH', '--markdown=FILEPATH', 'Specify where to export the markdown file') do |arg|
-        @options[:markdown_fileout] = arg
-      end
-
-      parser.on('-cFILEPATH', '--csv=FILEPATH', 'Specify where to export the csv file') do |arg|
-        @options[:csv_fileout] = arg
-      end
-
-      parser.on('--display-markdown', 'Displays markdown output to stdout') do
-        @options[:markdown_stdout] = true
-      end
-
-      parser.on('--display-csv', 'Displays cvs output to stdout') do
-        @options[:csv_stdout] = true
-      end
-
-      # parser.on("--csv-in=FILEPATH", "parses") do |arg|
-      #   @options[:csv_fileout] = arg
-      # end
-    end.parse!
-  end
+		# p csv_shapper
+		csv_shapper.to_csv
+	end
 end
-
-PSP.new
